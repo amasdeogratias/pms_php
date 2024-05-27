6138ebf419a32ce48669fb926ec6fdaf670ee440
fixnetcodes <deogratiasprog@gmail.com>
2024-05-27 12:54:07 +0300

    structured create company form

--- views/create-company.php
+++ views/create-company.php
@@ -1,56 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Estate 360 | Create Company</title>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assest/css/bootstrap.min.css" type="text/css" />
-    <link rel="stylesheet" href="assest/css/style.css" type="text/css" />
-    <link rel="stylesheet" href="assest/css/raise-questions.css" type="text/css" />
-</head>
-
-<body>
-    <div class="d-flex justify-content-between">
-        <?php include 'assest/include/sidebar.php'; ?>
+<?php
+include '../partials/header.php';
+?>
         <div class="dashboard-main">
             <header class="d-flex justify-content-between align-items-center">
                 <div class="add-back d-flex align-items-center">
                     <h1>Create Company</h1>
                 </div>
                 <div class="customer d-flex align-items-center">
-                    <a href="index.php" class="logout">
-                        <img src="assest/images/icons/logout.svg" alt="Logout" />
+                    <a href="../controllers/AuthController.php?f=Logout" class="logout">
+                        <img src="../assets/images/icons/logout.svg" alt="Logout" />
                     </a>
                     <a href="#" class="profile">
-                        <img src="assest/images/user-pic.jpg" alt="Profile" />
+                        <img src="../assets/images/user-pic.jpg" alt="Profile" />
                     </a>
                 </div>
             </header>
             <div class="quotationslist-main">
                 <!-- <h2>Individual Property Overview</h2> -->
                 <div class="form-main">
-                <form class="quotationform">
+                <form class="quotationform" method="post" id="createCompany">
                   <div class="d-flex justify-content-between customerbox mb-3">
                      <div class="field-box">
-                        <span>Company Name</span>  
+                        <span>Company Name</span>
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
+                           <input type="text" class="form-control" name="company_name" id="company_name"  placeholder="Enter company name">
                         </div>
                      </div>
                      <div class="field-box">
                         <span>Mailing Name</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
+                           <input type="text" class="form-control" name="mailing_name" id="mailing_name" placeholder="Enter mailing name">
                         </div>
                      </div>                     
                      <div class="field-box">
                         <span>Address</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
+                           <input type="text" class="form-control" name="address" id="address" placeholder="Enter address">
                         </div>
                      </div>                    
                   </div>
@@ -58,19 +43,19 @@
                   <div class="field-box">
                         <span>District</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
+                           <input type="text" class="form-control" name="district" id="district" placeholder="Enter district">
                         </div>
                      </div>
                      <div class="field-box">
                         <span>Country</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
+                           <input type="text" class="form-control" name="country" id="country" placeholder="Enter country">
                         </div>
                      </div>                     
                      <div class="field-box">
                         <span>Pincode</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
+                           <input type="text" class="form-control" name="pincode" id="pincode" placeholder="Enter pincode">
                         </div>
                      </div>                    
                   </div>
@@ -78,19 +63,19 @@
                      <div class="field-box">
                         <span>Telephone No.</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
+                           <input type="text" class="form-control" name="telephone" id="telephone" placeholder="Enter telephone">
                         </div>
                      </div>
                      <div class="field-box">
                         <span>Mobile No.</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Mobile">                                                      
+                           <input type="text" class="form-control" name="mobile" id="mobile" placeholder="Enter mobile">
                         </div>
                      </div>
                      <div class="field-box">
                         <span>Fax No.</span>  
                         <div class="d-flex justify-content-between">
-                        <input class="form-control" type="Text" placeholder="Fax"> 
+                        <input type="text" class="form-control" name="fax" id="fax" placeholder="Enter fax">
                         </div>
                      </div>
                   </div>
@@ -98,19 +83,19 @@
                      <div class="field-box">
                         <span>Email Id</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
+                           <input type="email" class="form-control" name="email" id="email" placeholder="Enter email">
                         </div>
                      </div>
                      <div class="field-box">
                         <span>Website</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
+                           <input type="text" class="form-control" name="website" id="website" placeholder="Enter website link">
                         </div>
                      </div>
                      <div class="field-box">
                         <span>Company Start Date</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="date">                                                      
+                           <input type="date" class="form-control" name="start_date" id="start_date" placeholder="Enter company start date">
                         </div>
                      </div>
                   </div>
@@ -125,99 +110,17 @@
                      <div class="field-box">
                         <span>Admin Password</span>  
                         <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
+                           <input type="password" class="form-control" placeholder="Text">
                         </div>
                      </div>
                   
                   </div>
 
-
-                  <!-- <div class="d-flex justify-content-between customerbox mb-3">
-                     <div class="field-box">
-                        <span>Change in Existing Leased Property ?</span>  
-                        <div class="d-flex checkgap">
-                           <div class="form-check">
-                              <input class="form-check-input" type="checkbox" value="" id="Yes">
-                              <label class="form-check-label" for="Yes">
-                              Yes
-                              </label>
-                           </div>
-                           <div class="form-check">
-                              <input class="form-check-input" type="checkbox" value="" id="No">
-                              <label class="form-check-label" for="No">
-                              No
-                              </label>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="field-box">
-                        <span>Provision to Increase/Decrease Rent ?</span>  
-                        <div class="d-flex checkgap">
-                           <div class="form-check">
-                              <input class="form-check-input" type="checkbox" value="" id="yes1">
-                              <label class="form-check-label" for="yes1">
-                              Yes
-                              </label>
-                           </div>
-                           <div class="form-check">
-                              <input class="form-check-input" type="checkbox" value="" id="no1">
-                              <label class="form-check-label" for="no1">
-                              No
-                              </label>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="field-box">
-                        <span>Option to Select Multiple Unit Shop ?</span>  
-                        <div class="d-flex checkgap">
-                           <div class="form-check">
-                              <input class="form-check-input" type="checkbox" value="" id="yes2">
-                              <label class="form-check-label" for="yes2">
-                              Yes
-                              </label>
-                           </div>
-                           <div class="form-check">
-                              <input class="form-check-input" type="checkbox" value="" id="no2">
-                              <label class="form-check-label" for="no2">
-                              No
-                              </label>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="d-flex justify-content-between customerbox mb-3">
-                     <div class="field-box">
-                        <span>Additional Property Requirement</span>  
-                        <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
-                        </div>
-                     </div>
-                     <div class="field-box">
-                        <span>Additional Property Requirement</span>  
-                        <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                      
-                        </div>
-                     </div>
-                     <div class="field-box">
-                        <span>Additional Property Requirement</span>  
-                        <div class="d-flex justify-content-between">
-                           <input class="form-control" type="text" placeholder="Text">                                                                                                          
-                        </div>
-                     </div>
-                  </div> -->
                   <div class="field-box d-flex justify-content-end submit-btn">
                      <button type="button" class="btn btn-gray">Reset</button>
-                     <button type="button" class="btn btn-primary">Save</button>
+                     <button type="button" id="submit" class="btn btn-primary">Save</button>
                   </div>
                </form>
                 </div>
             </div>
-        </div>
-
-        <script src="assest/js/jquery-3.7.1.min.js"></script>
-        <script src="assest/js/bootstrap.bundle.min.js"></script> 
-        <script src="assest/js/comon.js"></script>     
-
-</body>
-
-</html>
+<?php include ('../partials/footer.php'); ?>
