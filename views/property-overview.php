7fd22b8c631a49f569694ca4aa4fa81809ba1d57
fixnetcodes <deogratiasprog@gmail.com>
2024-05-30 13:47:54 +0300

    property overview design

--- views/property-overview.php
+++ views/property-overview.php
@@ -1,45 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Estate 360 | Property Management</title>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="assest/css/bootstrap.min.css" type="text/css" />
-      <link rel="stylesheet" href="assest/css/select.min.css" type="text/css" />
-      <link rel="stylesheet" href="assest/css/style.css" type="text/css" />
-      <link rel="stylesheet" href="assest/css/raise-questions.css" type="text/css" />
-   </head>
-   <body>
-      <div class="d-flex justify-content-between formback">
-      <?php include 'assest/include/sidebar.php'; ?>
+<?php
+$subtitle = "Property Management";
+include '../partials/header.php';
+?>
+
       <div class="dashboard-main">
          <header class="d-flex justify-content-between align-items-center">
             <div class="add-back d-flex align-items-center">
-               <a href="property.php" class="back btn"><img src="assest/images/icons/BackArrow.svg" alt="Back"/> Back </a> 
+               <a href="property.php" class="back btn"><img src="../assets/images/icons/BackArrow.svg" alt="Back"/> Back </a>
                <h1>Property Management</h1>
             </div>
             <div class="customer d-flex align-items-center">
                <div class="search-container">
                   <form action="#" method="get">
                      <input class="search expandright" id="searchright" type="search" name="q" placeholder="Search">
-                     <label class="button searchbutton" for="searchright"><span class="mglass"><img src="assest/images/icons/search-header.svg" alt="search"/></span></label>
+                     <label class="button searchbutton" for="searchright"><span class="mglass"><img src="../assets/images/icons/search-header.svg" alt="search"/></span></label>
                   </form>
                </div>
                <a href="index.php" class="logout">
-               <img src="assest/images/icons/logout.svg" alt="Logout"/>  
+               <img src="../assets/images/icons/logout.svg" alt="Logout"/>
                </a>
                <a href="#" class="profile">
-               <img src="assest/images/user-pic.jpg" alt="Profile"/>  
+               <img src="../assets/images/user-pic.jpg" alt="Profile"/>
                </a>
             </div>
          </header>
          <div class="quotationslist-main property-page" style="margin-top:30px;">
             <!-- <h3 class="title3">Sun WestBank</h3> -->
             <div class="property-picture">
-               <img src="assest/images/classroom.jpg" class="img-fluid" alt="buildings"/>                
+               <img src="../assets/images/classroom.jpg" class="img-fluid" alt="buildings"/>
             </div>
             <div class="form-main overview">
                <form class="quotationform">
@@ -159,10 +147,7 @@
             </div>
          </div>
       </div>
-      <script src="assest/js/jquery-3.7.1.min.js"></script>
-      <script src="assest/js/bootstrap.bundle.min.js"></script>       
-      <script src="assest/js/choices.min.js"></script>
-      <script src="assest/js/comon.js"></script>
+  <?php include ('../partials/footer.php'); ?>
       <script>
          $(document).ready(function(){    
              var multipleCancelButton = new Choices('#choices-multiple-remove-button', {
@@ -173,6 +158,3 @@
              });   
          });
       </script>
-   
-   </body>
-</html>
