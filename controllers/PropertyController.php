5b24509da3582417d9bbeb846ea9e9a7fea70a5d
fixnetcodes <deogratiasprog@gmail.com>
2024-06-01 11:54:07 +0300

    controller to fetch property types

--- controllers/PropertyController.php
+++ controllers/PropertyController.php
@@ -1,1 +1,27 @@
 <?php
+session_start();
+error_reporting(0);
+require('../database/connection.php');
+require('../models/Property.php');
+$database = new Database();
+$db = $database->connect();
+$property = new Property($db);
+
+
+
+
+function getPropertyType()
+{
+    global $property;
+    $property_type = $_POST['property_type'];
+    if (isset($property_type)){
+        $results = $property->fetchPropertyType($property_type);
+        if ($results){
+            echo json_encode($results);
+        }
+    }
+}
+
+
+$f = $_GET['f'];
+$f();
